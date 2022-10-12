86b834ae72b6fa19d37d536ba2834db7e15af62c
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2022-10-12 13:09:55 +0000

    [Finish]4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
     about_path
   end
 
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
   protected
 
   def configure_permitted_parameters
