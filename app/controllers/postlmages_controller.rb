95238004967cbd646c07f3318fd40d1b97cac72d
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-10-13 15:51:07 +0000

    [Remove]4-10

--- app/controllers/postlmages_controller.rb
+++ app/controllers/postlmages_controller.rb
@@ -1,10 +1,29 @@
 class PostlmagesController < ApplicationController
   def new
+    @postimage=Postimage.new
   end
+
+def create
+  @postimage=Postimage.new(postimage_params)
+  @postimage.user_id=current_user.id
+  @postimage.save
+  redirect_to postimages_path
+end
 
   def index
   end
 
   def show
   end
+
+  def destroy
+  end
+
+
+  private
+
+  def postimage_params
+    params.require(:postimage).permit(:shop_name,:image,:caption)
+  end
+
 end
