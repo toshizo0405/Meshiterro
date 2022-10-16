b16de22f12a515e134fa7b77193968b3f1ac2b9d
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-10-16 14:28:08 +0000

    [Add]4-11

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,14 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
+   def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
+
 end
