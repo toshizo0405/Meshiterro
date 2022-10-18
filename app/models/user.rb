d8ecec4e37085861bc450fffbad6745ca170ba97
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-10-18 14:06:21 +0000

    [Finish]4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :post_images,dependent: :destroy
+
+  has_one_attached :profile_image
+
+def get_profile_image(width,height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+end
+
 end
