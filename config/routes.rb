3b45998506439c10bae76f528f24cf5ca2120592
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-10-18 13:33:28 +0000

    [Finish]4-1３

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :post_images, only: [:new,:create,:index,:show]
+  resources :post_images, only: [:new,:create,:index,:show,:destroy]
 
   get 'homes/about' =>'homes#about' ,as: 'about'
 
