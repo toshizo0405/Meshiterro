2ef6ca56d99168c86c4ff0a130205b3675c3d6bb
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-10-22 13:50:34 +0000

    [Add]4-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
   def edit
     @user=User.find(params[:id])
   end
+  
+  
+  def update
+    @user = @User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+  
+  
 end
