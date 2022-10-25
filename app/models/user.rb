bc371a3d0b183fda3810dde45a10014ca6d2da82
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2022-10-25 15:12:02 +0000

    [Add]4-19

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :post_images,dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
 def get_profile_image(width,height)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :post_images, only: [:new,:create,:index,:show,:destroy]do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments,only: [:create,:destroy]
   end
   resources :users, only: [:show, :edit,:update]
