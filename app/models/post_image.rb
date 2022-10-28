0d7a08fa053ed4384bdb743fdb7e3d612b32783c
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2022-10-28 12:11:06 +0000

    [Finish]4-21

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,9 @@ class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  validates :shop_name,presence: true
+  validates :image,presence: true
 
    def get_image
     unless image.attached?
